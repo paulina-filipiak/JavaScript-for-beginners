b3cf8dec4be853bf870fbb4e5d85b422f215d6e5
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-09-01 12:41:05 +0200

    learned how to use break and continue keywords

--- test.js
+++ test.js
@@ -1,8 +1,12 @@
-var links = document.getElementsByTagName("a");
+for (i = 0; i < 10; i++) {
 
-for (i = 1; i <= links.length; i++) {
-    console.log("this is link number" + i);
+    if(i === 5 || i === 3){
+        continue;
+    }
 
+    console.log(i);
+
+    if (i === 7){
+        break;
+    }
 }
-
-document.write("all links are now looped");
