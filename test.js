0d6db8cd70628f546ce928d5190f6619882fe8d4
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-09-01 13:48:42 +0200

    learned about local and global variables

--- test.js
+++ test.js
@@ -1,7 +1,13 @@
 function getAverage (a,b){
     var average = (a + b) / 2;
+    console.log(average);
     return average;
 }
 
 var myResult = getAverage(5,8);
-console.log("the average is " + myResult);
+
+function logResult(){
+    console.log("the average is " + myResult);
+}
+
+logResult();
