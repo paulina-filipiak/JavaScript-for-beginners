06d1a9f22e94f47af5a6b18d748d697999ad8b72
Paulina Filipiak <slodzinska.paulina@gmail.com>
2022-09-02 21:45:38 +0200

    created onclick event to shrink or expand text box

--- test.js
+++ test.js
@@ -1,21 +1,12 @@
-var Car = function(maxSpeed, driver){
-    this.maxSpeed = maxSpeed;
-    this.driver = driver;
-    this.drive = function(speed, time){
-        console.log(speed * time) ;
-    };
-    this.logDriver = function(){
-        console.log("driver name is " + this.driver);
-    };
-}
+var content = document.getElementById("content");
+var button = document.getElementById("show-more");
 
-var myCar = new Car(70, "Bartek");
-var myCar2 = new Car(60, "Paweł");
-var myCar3 = new Car(90, "Krzysiek");
-var myCar4 = new Car(20, "lalala");
+button.onclick = function(){
+    if(content.className == "open"){
+        content.className = "";
 
-myCar.drive(30,5);
-
-myCar4.logDriver();
-
-
+    } else{
+        content.className = "open";
+        button.innerHTML = "show less";
+    }
+};
